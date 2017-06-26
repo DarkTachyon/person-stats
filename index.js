77c3a3bc8e99ea0238b7e65615bfec7467c194b7
Alex Jackson <tmb.alexander@gmail.com>
2017-06-26 18:38:28 -0400

    Completed Super Mega Bonus Credit Hyper Fighting

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-function changeHeading(ev)
+function changeHeading(ev, er)
 {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
+    const some = f.something.value
     //debugger
-    const heading = document.querySelector('h1');
-    heading.textContent = name
-
+    const div = document.querySelector('div#stats')
+    const paraDiv = document.querySelector('#div-para')
+    //div.textContent = name
+    paraDiv.textContent = name + ' ' + some
+    //debugger
+    if (name === 'blue' || some === 'blue')
+        paraDiv.style.color = 'blue'
 }
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
