2cb5c8ff9dd33f8a68f8f20bdb3c375d1a7e471a
Alex Jackson <tmb.alexander@gmail.com>
2017-06-27 13:54:55 -0400

    Add new form for color to make it work this time....

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function changeHeading(ev, er)
     const f = ev.target
     const name = f.personName.value
     const some = f.something.value
+    const color = f.color.value
     //debugger
     const div = document.querySelector('div#stats')
     const paraDiv = document.querySelector('#div-para')
     //div.textContent = name
     paraDiv.textContent = name + ' ' + some
     //debugger
-    if (name === 'blue' || some === 'blue')
-        paraDiv.style.color = 'blue'
+    paraDiv.style.color = color
 }
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeHeading)
