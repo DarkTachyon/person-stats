c2e8f0d3eb5ae720619b320c71ff9860a4b5c545
DarkTachyon <tmb.alexander@gmail.com>
2017-06-27 21:44:16 -0400

    made changeHeading handleSubmit, add renderColor, renderListItem, renderList

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-function changeHeading(ev, er)
+function renderColor(favColor)
+{
+    const tempDiv = document.createElement('div')
+    tempDiv.style.backgroundColor = favColor
+    tempDiv.style.width = '6rem'
+    tempDiv.style.height = '3rem'
+
+    return tempDiv
+}
+
+function renderListItem(listText)
+{
+    const tempItem = document.createElement('li')
+    tempItem.textContent = listText
+
+    return tempItem
+}
+
+function renderList(name, age, color)
+{
+    const tempList = document.createElement('ul')
+
+    const nameItem = renderListItem(`Name: ${name}`)
+    tempList.appendChild(nameItem)
+
+    const ageItem = renderListItem(`Age: ${age}`)
+    tempList.appendChild(ageItem)
+
+    const colorItem = renderListItem(`Color: ${color}`)
+
+    const colorDiv = renderColor(color)
+    colorItem.appendChild(colorDiv)
+
+    tempList.appendChild(colorItem)
+
+    return tempList
+}
+
+function handleSubmit(ev)
 {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
-    const some = f.something.value
+    const age = f.personAge.value
     const color = f.color.value
-    //debugger
+
     const div = document.querySelector('div#stats')
-    const paraDiv = document.querySelector('#div-para')
-    //can be done with document.querySelector(#stats p or #stats > p)
-    //div.textContent = name
-    paraDiv.textContent = `${name} ${some}`
-    //debugger
-    paraDiv.style.color = color
+
+    const list = renderList(name, age, color)
+
+    div.appendChild(list)
 }
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', changeHeading)
+personForm.addEventListener('submit', handleSubmit)
